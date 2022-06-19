e1e86a032dd5b3ed3f28f0339bdf5f946a06ed4b
Ruby <=>
2022-06-20 02:15:50 +0545

    Error solved

--- lib/main.dart
+++ lib/main.dart
@@ -25,32 +25,41 @@ class RubyApp extends StatelessWidget {
       title: 'Column and Row',
       debugShowCheckedModeBanner: false,
       home: SafeArea(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.grey[300],
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.red,
-              ),
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.green,
-              ),
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.blue,
-              )
-            ],
+        child: Home(),
+      ),
+    );
+  }
+}
+
+class RubyRowColumn extends StatelessWidget {
+  const RubyRowColumn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.grey[300],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.red,
           ),
-        ),
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.green,
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.blue,
+          )
+        ],
       ),
     );
   }
